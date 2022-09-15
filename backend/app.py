278f8f70a06b0510dc642b957301f02e8344a177
dima <hibdhz221@gmail.com>
2022-09-15 17:23:37 +0300

    basic env created

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/create-basic-topology')
+@app.route('/', methods=['POST', 'GET'])
 def creation():
     return 'success'
 
 
 if __name__ == '__main__':
+    app.env = 'development'
     app.run(debug=True, port=5001)
