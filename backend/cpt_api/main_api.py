54cfbd1cca5488a30d80478d6764a2d0569b66cc
FreazeMood <hibdhz221@gmail.com>
2022-09-17 00:59:15 +0300

    added screen class and running cpt from the same process

--- backend/cpt_api/main_api.py
+++ backend/cpt_api/main_api.py
@@ -1,24 +1,45 @@
 import os
-import subprocess
 
-class Cisco_Packet_Tracer:
 
-    def __init__(self, connection):
+class Screen:
 
-        self.connection = self.make_conn(connection)
-        self.cursor = self.get_cursor()
+    def __init__(self) -> None:
 
+        self.resolution = self.define_screen_size()
+
+    def define_screen_size(self):
+        return 'resolution'
+
+
+class Cisco_Packet_Tracer(Screen):
+
+    def __init__(self, connection) -> None:
+        super().__init__()
+        self.is_opened = self.make_conn(connection)
+        self.window = self.get_window()
 
     def make_conn(self, dir) -> 'connection':
         os.chdir('D:')
-        subprocess.call([dir])
 
+        try:
+            os.startfile(dir)
+            return True
 
-    def get_cursor(self):
-        
-        if not self.connection:
+        except FileNotFoundError as e:
+            print(
+                '===============issue with the path check if its correct================')
+            raise e
+
+    def get_window(self):
+
+        if not self.is_opened:
             raise TypeError
 
+        print('===============window opened successfully==================')
+        print(self.resolution)
+
+
 if __name__ == '__main__':
-    cpt = Cisco_Packet_Tracer('D:\\pc shit\\Cisco Packet Tracer 8.0\\bin\\PacketTracer.exe')
-    print(cpt.connection)
+    cpt = Cisco_Packet_Tracer(
+        'D:\\pc shit\\Cisco Packet Tracer 8.0\\bin\\PacketTracer.exe')
+    print(f"window in opened state:\n{cpt.is_opened}")
