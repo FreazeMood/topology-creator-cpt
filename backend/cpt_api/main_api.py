2ea0b11cae93bc2d63ae82044a60f648a7ce0c46
FreazeMood <hibdhz221@gmail.com>
2022-09-28 18:27:47 +0300

    added script to wait until the window is initialized

--- backend/cpt_api/main_api.py
+++ backend/cpt_api/main_api.py
@@ -2,16 +2,18 @@ import os
 import psutil
 import platform
 import pyautogui as pya
+from constants import PATH_ERROR
 import time
+
 
 
 class Cisco_Packet_Tracer:
 
     def __init__(self, directory) -> None:
-        self.directory = directory
+        self.directory = directory # the executable file of the packet tracer
         self.os = self.get_os()
-        self.is_opened = self.open_window(directory)
-        print(f'''===============window opened successfully==================
+        self.is_opened = self.open_window(directory) # check if the application is running else start it
+        print(f'''===============Application successfully started==================
 window in opened state: {self.is_opened}
 current os is: {self.os}
 ''')
@@ -20,9 +22,10 @@ current os is: {self.os}
     mac_os_names = ('Mac', 'Darwin', 'Os2', 'Os2emx')
 
     def open_window(self, directory):
-        
-        if self.os in self.win_os_names: #  adaptation for windows
-            if os.getcwd()[0] != self.directory[0]: #  check if the current directory is the directory where's the program 
+
+        if self.os in self.win_os_names:  # adaptation for windows
+            # check if the current directory is the directory where's the program
+            if os.getcwd()[0] != self.directory[0]:
                 print(f"changing directory to: {self.directory[0:2]}")
                 os.chdir(self.directory[0:2])
 
@@ -30,32 +33,27 @@ current os is: {self.os}
                 if not self.packet_tracer_is_running():
                     os.startfile(directory)
                     return True
-                
+
                 return True
 
             except FileNotFoundError as e:
-                print(
-                    '===============issue with the path check if its correct================'
-                    )
+                print(PATH_ERROR)
                 raise e
 
-        if self.os in self.mac_os_names: #  adaptation for macOS
-            # @TODO: 
-            #       raise a file not found error if os.system wasnt executed 
+        if self.os in self.mac_os_names:  # adaptation for macOS
+            # @TODO:
+            #       raise a file not found error if os.system wasnt executed
             try:
                 if not self.packet_tracer_is_running():
                     app = self.directory.split('/')[-1]
                     os.system(f"open -a '{app}'")
                     return True
-                
+
                 return True
 
             except FileNotFoundError as e:
-                print(
-                    '===============issue with the path check if its correct================'
-                    )
+                print(PATH_ERROR)
                 raise e
-
 
     def get_os(self):
         return platform.uname().system
@@ -80,7 +78,7 @@ class Window(Cisco_Packet_Tracer):
 
     def define_screen_size(self):
         return f'{pya.size().width} * {pya.size().height}'
-        
+
     def check_if_window_on_screen(self):
 
         if self.os in self.win_os_names and self.packet_tracer_is_running:
@@ -89,8 +87,8 @@ class Window(Cisco_Packet_Tracer):
             import ctypes
             foreground_window = win32gui.GetForegroundWindow()
             active_window_name = win32gui.GetWindowText(foreground_window)
-            self.window = win32gui.FindWindow(None ,'Cisco Packet Tracer')
-            
+            self.window = win32gui.FindWindow(None, 'Cisco Packet Tracer')
+
             if not active_window_name == 'Cisco Packet Tracer':
                 user32 = ctypes.windll.user32
                 user32.SetForegroundWindow(self.window)
@@ -98,22 +96,31 @@ class Window(Cisco_Packet_Tracer):
                 if user32.IsIconic(self.window):
                     user32.ShowWindow(self.window, 9)
 
-            return True  
+                return True
+
+            return True
 
     def find_window_resolution(self):
 
         if self.os in self.win_os_names:
 
             import win32gui
-            rect = win32gui.GetWindowRect(self.window)
-            x = rect[0]
-            y = rect[1]
-            w = rect[2] - x
-            h = rect[3] - y
-            print("The window is %s:" % win32gui.GetWindowText(self.window))
-            print("\tLocation: (%d, %d)" % (x, y))
-            print("\t    Size: (%d, %d)" % (w, h))
-            return {"location": (x, y), "size": (w, h)}
+            import pywintypes
+            try:
+                rect = win32gui.GetWindowRect(self.window)
+                x = rect[0]
+                y = rect[1]
+                w = rect[2] - x
+                h = rect[3] - y
+                print("The window is %s:" % win32gui.GetWindowText(self.window))
+                print("\tLocation: (%d, %d)" % (x, y))
+                print("\t    Size: (%d, %d)" % (w, h))
+                return {"location": (x, y), "size": (w, h)}
+            
+            except pywintypes.error:
+                time.sleep(3)
+                self.check_if_window_on_screen()
+                self.find_window_resolution()
 
 
 if __name__ == '__main__':
